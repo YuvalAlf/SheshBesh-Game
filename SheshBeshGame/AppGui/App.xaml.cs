17219cc922ecd187c1b9ebe0f7f9a4cdb7ffded5
YuvalAlf <u67v67@gmail.com>
2016-11-19 20:37:19 +0200

    Project folders init

---
+++ SheshBeshGame/AppGui/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace SheshBeshGame.AppGui
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
